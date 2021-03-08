c451e6874609fc521aef4b485e71793feece8156
Lizette Ayala <ayall291@newschool.edu>
2021-03-08 10:24:12 -0600

    Add files via upload

---
+++ global.js
@@ -0,0 +1,244 @@
+const container = document.getElementById("slider");
+const images = document.getElementsByClassName("img");
+
+$(document).ready(function() { 
+ $(".hover-background:nth-child(1)").click(function() {
+         $("#bio-01").show();
+         $("#bio-02").hide();
+         $("#bio-03").hide();
+         $("#bio-04").hide();
+         $("#bio-05").hide();
+         $("#bio-06").hide();
+         $("#bio-07").hide();
+         $("#bio-08").hide();
+         $("#bio-09").hide();
+         $("#bio-10").hide();
+         $("#bio-11").hide();
+         $("#bio-12").hide();
+         $("#bio-13").hide();
+   }); 
+ }); 
+
+$(document).ready(function() { 
+ $(".hover-background:nth-child(2)").click(function() {
+         $("#bio-02").show();
+         $("#bio-01").hide();
+         $("#bio-03").hide();
+         $("#bio-04").hide();
+         $("#bio-05").hide();
+         $("#bio-06").hide();
+         $("#bio-07").hide();
+         $("#bio-08").hide();
+         $("#bio-09").hide();
+         $("#bio-10").hide();
+         $("#bio-11").hide();
+         $("#bio-12").hide();
+         $("#bio-13").hide();
+   }); 
+ }); 
+
+$(document).ready(function() { 
+ $(".hover-background:nth-child(3)").click(function() {
+         $("#bio-03").show();
+         $("#bio-01").hide();
+         $("#bio-02").hide();
+         $("#bio-04").hide();
+         $("#bio-05").hide();
+         $("#bio-06").hide();
+         $("#bio-07").hide();
+         $("#bio-08").hide();
+         $("#bio-09").hide();
+         $("#bio-10").hide();
+         $("#bio-11").hide();
+         $("#bio-12").hide();
+         $("#bio-13").hide();
+   }); 
+ }); 
+
+$(document).ready(function() { 
+ $(".hover-background:nth-child(4)").click(function() {
+         $("#bio-04").show();
+         $("#bio-01").hide();
+         $("#bio-02").hide();
+         $("#bio-03").hide();
+         $("#bio-05").hide();
+         $("#bio-06").hide();
+         $("#bio-07").hide();
+         $("#bio-08").hide();
+         $("#bio-09").hide();
+         $("#bio-10").hide();
+         $("#bio-11").hide();
+         $("#bio-12").hide();
+         $("#bio-13").hide();
+   }); 
+ }); 
+
+$(document).ready(function() { 
+ $(".hover-background:nth-child(5)").click(function() {
+         $("#bio-05").show();
+         $("#bio-01").hide();
+         $("#bio-02").hide();
+         $("#bio-03").hide();
+         $("#bio-04").hide();
+         $("#bio-06").hide();
+         $("#bio-07").hide();
+         $("#bio-08").hide();
+         $("#bio-09").hide();
+         $("#bio-10").hide();
+         $("#bio-11").hide();
+         $("#bio-12").hide();
+         $("#bio-13").hide();
+   }); 
+ }); 
+
+$(document).ready(function() { 
+ $(".hover-background:nth-child(6)").click(function() {
+         $("#bio-06").show();
+         $("#bio-01").hide();
+         $("#bio-02").hide();
+         $("#bio-03").hide();
+         $("#bio-04").hide();
+         $("#bio-05").hide();
+         $("#bio-07").hide();
+         $("#bio-08").hide();
+         $("#bio-09").hide();
+         $("#bio-10").hide();
+         $("#bio-11").hide();
+         $("#bio-12").hide();
+         $("#bio-13").hide();
+
+   }); 
+ }); 
+
+$(document).ready(function() { 
+ $(".hover-background:nth-child(7)").click(function() {
+         $("#bio-07").show();
+         $("#bio-01").hide();
+         $("#bio-02").hide();
+         $("#bio-03").hide();
+         $("#bio-04").hide();
+         $("#bio-05").hide();
+         $("#bio-06").hide();
+         $("#bio-08").hide();
+         $("#bio-09").hide();
+         $("#bio-10").hide();
+         $("#bio-11").hide();
+         $("#bio-12").hide();
+         $("#bio-13").hide();
+   }); 
+ }); 
+
+$(document).ready(function() { 
+ $(".hover-background:nth-child(8)").click(function() {
+         $("#bio-08").show();
+         $("#bio-01").hide();
+         $("#bio-02").hide();
+         $("#bio-03").hide();
+         $("#bio-04").hide();
+         $("#bio-05").hide();
+         $("#bio-06").hide();
+         $("#bio-07").hide();
+         $("#bio-09").hide();
+         $("#bio-10").hide();
+         $("#bio-11").hide();
+         $("#bio-12").hide();
+         $("#bio-13").hide();
+   }); 
+ }); 
+
+$(document).ready(function() { 
+ $(".hover-background:nth-child(9)").click(function() {
+         $("#bio-09").show();
+         $("#bio-01").hide();
+         $("#bio-02").hide();
+         $("#bio-03").hide();
+         $("#bio-04").hide();
+         $("#bio-05").hide();
+         $("#bio-06").hide();
+         $("#bio-07").hide();
+         $("#bio-08").hide();
+         $("#bio-10").hide();
+         $("#bio-11").hide();
+         $("#bio-12").hide();
+         $("#bio-13").hide();
+   }); 
+ }); 
+
+$(document).ready(function() { 
+ $(".hover-background:nth-child(10)").click(function() {
+         $("#bio-10").show();
+         $("#bio-01").hide();
+         $("#bio-02").hide();
+         $("#bio-03").hide();
+         $("#bio-04").hide();
+         $("#bio-05").hide();
+         $("#bio-06").hide();
+         $("#bio-07").hide();
+         $("#bio-08").hide();
+         $("#bio-09").hide();
+         $("#bio-11").hide();
+         $("#bio-12").hide();
+         $("#bio-13").hide();
+   }); 
+ }); 
+
+$(document).ready(function() { 
+ $(".hover-background:nth-child(11)").click(function() {
+         $("#bio-11").show();
+         $("#bio-01").hide();
+         $("#bio-02").hide();
+         $("#bio-03").hide();
+         $("#bio-04").hide();
+         $("#bio-05").hide();
+         $("#bio-06").hide();
+         $("#bio-07").hide();
+         $("#bio-08").hide();
+         $("#bio-09").hide();
+         $("#bio-10").hide();
+         $("#bio-12").hide();
+         $("#bio-13").hide();
+   }); 
+ }); 
+
+$(document).ready(function() { 
+ $(".hover-background:nth-child(12)").click(function() {
+         $("#bio-12").show();
+         $("#bio-01").hide();
+         $("#bio-02").hide();
+         $("#bio-03").hide();
+         $("#bio-04").hide();
+         $("#bio-05").hide();
+         $("#bio-06").hide();
+         $("#bio-07").hide();
+         $("#bio-08").hide();
+         $("#bio-09").hide();
+         $("#bio-10").hide();
+         $("#bio-11").hide();
+         $("#bio-13").hide();
+   }); 
+ }); 
+
+$(document).ready(function() { 
+ $(".hover-background:nth-child(13)").click(function() {
+         $("#bio-13").show();
+         $("#bio-01").hide();
+         $("#bio-02").hide();
+         $("#bio-03").hide();
+         $("#bio-04").hide();
+         $("#bio-05").hide();
+         $("#bio-06").hide();
+         $("#bio-07").hide();
+         $("#bio-08").hide();
+         $("#bio-09").hide();
+         $("#bio-10").hide();
+         $("#bio-11").hide();
+         $("#bio-12").hide();
+   }); 
+ }); 
+
+
+
+
+
+
+
